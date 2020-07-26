64b443cbf561f22df94f6e264b001a9f54f4ebd0
Nick Oelsner <noelsner@gmail.com>
2020-07-26 10:40:06 -0700

    added language dropdown, added video and hubspot to react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React, { useState } from 'react';
-import FileUpload from './components/FileUpload';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { languageOptions, contentVariations } from './LanguageContent';
+import { ReactComponent as LanguagePicker } from './components/LanguagePicker.svg';
+import HubspotForm from './components/HubspotForm';
 
 function App() {
 
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [document, setDocument] = useState('');
+  const [language, setLanguage] = useState('en')
+  
+  
+  const browserLanguage = window.navigator.userLanguage || window.navigator.language;
+   
+  useEffect(() => {
+    if (browserLanguage){
+      setLanguage(browserLanguage.substring(0,2))
+    }
+  },[browserLanguage])
 
-  const submitForm = (user) => {
-    axios.post('http://localhost:5000/api/users/add', user)
-      .then(response => {
-        console.log('response:', response.data)
-      })
-  };
+  console.log('browserLanguage :>> ', browserLanguage);
 
+  
   return (
-    <div>
-      <h1>e-immigrate</h1>
-      <FileUpload submitForm={submitForm} phoneNumber={phoneNumber} setPhoneNumber={setPhoneNumber} document={document} setDocument={setDocument} />
+    <div className='container d-flex flex-column align-items-center justify-content-center pt-4'>
+
+      <h3>{ contentVariations[language].language }:</h3>
+      <div className="btn-group">
+        <button type="button" className="btn btn-md btn-primary dropdown-toggle d-flex align-items-center" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <LanguagePicker height='2rem' width='2rem' className='mr-2 '/>
+          <span className='mr-2 d-flex-row align-items-center' style={ { verticalAlign: 'middle' }}>
+            { contentVariations[language].value }
+          </span>
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          { 
+            languageOptions.map( (lang, idx) => {
+              return (
+                <button className='dropdown-item' value={lang.code} key={idx} onClick={ev => setLanguage(ev.target.value)} >{lang.full}</button>
+              )
+            })
+          }
+        </div>
+      </div>
+
+      <div className='my-5 mx-4 embed-responsive embed-responsive-16by9'>
+        <iframe width="560" height="315" src={ contentVariations[language].video } title='Project New Citizen Video' frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+      
+      <HubspotForm hubspotFormId={ contentVariations[language].hubspotFormId } />
+      
     </div>
   );
 }
---
+++ src/LanguageContent.js
@@ -0,0 +1,35 @@
+export const languageOptions = [
+  {
+    code: 'en',
+    full: 'English'
+  },
+  {
+    code: 'es',
+    full: 'Español'
+  },
+  {
+    code: 'vi',
+    full: 'Tiếng Việt'
+  },
+]
+
+export const contentVariations = {
+  en: {
+    value: 'English',
+    language: 'Preferred Language',
+    video: 'https://www.youtube.com/embed/QOt3Jc59BD4',
+    hubspotFormId: '2245b12f-d7e2-4828-9346-1da44664495e'
+  },
+  es: {
+    value: 'Español',
+    language: 'Idioma Preferido',
+    video: 'https://www.youtube.com/embed/QOt3Jc59BD4',
+    hubspotFormId: '2245b12f-d7e2-4828-9346-1da44664495e'
+  },
+  vi: {
+    value: 'Tiếng Việt',
+    language: 'Ngôn Ngữ Ưa Thích',
+    video: 'https://www.youtube.com/embed/MC2MEjzEZ2Y',
+    hubspotFormId: '2245b12f-d7e2-4828-9346-1da44664495e'
+  }
+};
