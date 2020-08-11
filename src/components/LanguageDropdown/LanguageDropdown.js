27f3b31036b8e9589fbb7394ea50f097517545ca
Nick Oelsner <noelsner@gmail.com>
2020-08-10 20:34:51 -0700

    changed language dropdown to grid, fixed a few issues

--- src/components/LanguageDropdown/LanguageDropdown.js
+++ src/components/LanguageDropdown/LanguageDropdown.js
@@ -4,20 +4,30 @@ import { languageOptions } from '../../data/LanguageContent';
 
 import './LanguageDropdown.css';
 
+const LanguageSelect = ({ setLanguage }) => {
+  return (
+    <div className='languageSelectContainer'>
+      <LanguagePicker className='languageIcon' height='2rem' width='2rem' />
+      <select onChange={ev => setLanguage(ev.target.value)} >
+        { 
+          languageOptions.map((lang, idx) => {
+            return (
+              <option key={idx} value={lang.code} >{lang.full}</option>
+            )
+          })
+        }
+      </select>
+    </div>
+  );
+};
+
 const LanguageDropdown = ({ language, setLanguage }) => {
   const { preferredLanguage } = language;
   return (
     <div className='languageDropdown'>
-      <div className='label'>
-        <LanguagePicker height='2rem' width='2rem' />
-        <h4>{ preferredLanguage }:</h4>
-      </div>
-      <div>
-        <select onChange={ev => setLanguage(ev.target.value)} >
-          { 
-            languageOptions.map((lang, idx) => <option key={idx} value={lang.code} >{lang.full}</option>)
-          }
-        </select>
+      <h4 className='preferredLanguage'>{ preferredLanguage }:</h4>
+      <div className='languageSelect'>
+        <LanguageSelect setLanguage={ setLanguage } />
       </div>
     </div>
   );
