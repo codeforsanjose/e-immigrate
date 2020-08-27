e840f60b7812e02831a8abb07892b762a2a2d598
Joseph Richardson <jrichardson@jmstudios.net>
2020-08-27 16:31:10 -0700

    some syntax updates to react hooks

--- src/components/auth/Admin.js
+++ src/components/auth/Admin.js
@@ -1,58 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginBox from './LoginBox.js';
 import RegisterBox from './RegisterBox.js';
-import './auth.css';
+import './Admin.css';
 
-class Admin extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoginOpen: true,
-            isRegisterOpen: false
-        }
+const Admin = (props) => {
+    const [adminState, setAdminState] = useState({
+        isLoginOpen: true,
+        isRegisterOpen: false,
+    })
+
+    const showLoginBox = () => {
+        setAdminState({ isLoginOpen: true, isRegisterOpen: false })
     }
 
-    showLoginBox() {
-        this.setState({ isLoginOpen: true, isRegisterOpen: false });
+    const showRegisterBox = () => {
+        setAdminState({ isRegisterOpen: true, isLoginOpen: false });
     }
 
-    showRegisterBox() {
-        this.setState({ isRegisterOpen: true, isLoginOpen: false });
-    }
+    const loginClassName = 'tab ' + adminState.isLoginOpen
+        ? 'selected-tab'
+        : ''
+    const registerClassName = 'tab ' + adminState.isRegisterOpen
+        ? 'selected-tab'
+        : ''
 
-    render() {
-
-        return (
-            <div>
-                <div className="box-controller">
-                    <div
-                        className={"controller " + (this.state.isLoginOpen
-                            ? "selected-controller"
-                            : "")}
-                        onClick={this
-                            .showLoginBox
-                            .bind(this)}>
-                        Login
-        </div>
-                    <div
-                        className={"controller " + (this.state.isRegisterOpen
-                            ? "selected-controller"
-                            : "")}
-                        onClick={this
-                            .showRegisterBox
-                            .bind(this)}>
-                        Register
-        </div>
+    const loginBox = adminState.isLoginOpen && <LoginBox />
+    const registerBox = adminState.isRegisterOpen && <RegisterBox />
+    return (
+        <div className="Admin">
+            <div className="admin-tab-nav">
+                <div
+                    className={ loginClassName }
+                    onClick={ showLoginBox }>
+                    Login
                 </div>
-
-                <div className="box-container">
-                    {this.state.isLoginOpen && <LoginBox />}
-                    {this.state.isRegisterOpen && <RegisterBox />}
+                <div
+                    className={ registerClassName }
+                    onClick={ showRegisterBox }>
+                    Register
                 </div>
             </div>
-        )
-    }
+
+            <div className="tab-container">
+                { loginBox }
+                { registerBox }
+            </div>
+        </div>
+    )
+    
 }
 
 export default Admin;
