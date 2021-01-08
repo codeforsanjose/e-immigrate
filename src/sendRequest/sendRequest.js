cb923c14244f2972908a4213efc3e37e996764c9
Joseph Richardson <jrichardson@jmstudios.net>
2021-01-07 19:22:01 -0800

    fixing dependecy changes

--- src/sendRequest/sendRequest.js
+++ src/sendRequest/sendRequest.js
@@ -1,12 +1,15 @@
 const DEFAULT_HEADERS = {
-    'Accept': 'application/json, text/plain, */*',
+    Accept: 'application/json, text/plain, */*',
     'Content-Type': 'application/json',
-}
+};
 
 const sendRequest = (requestObj, headers = DEFAULT_HEADERS) => {
     const url = requestObj.url;
     delete requestObj.url;
-    const response = fetch(url, { ...requestObj, ...headers }).then((data) => data.json());
+    const response = fetch(url, { ...requestObj, ...headers }).then((data) =>
+        data.json()
+    );
+
     return Promise.resolve(response);
 };
 
