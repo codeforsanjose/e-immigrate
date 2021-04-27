d17aed2181579d030542b093c15cec9fa2422e41
Nick Oelsner <noelsner@gmail.com>
2021-04-26 21:35:54 -0700

    update footer test

--- src/compositions/Footer/Footer.test.js
+++ src/compositions/Footer/Footer.test.js
@@ -3,7 +3,12 @@ import Footer from './Footer';
 
 import renderer from 'react-test-renderer';
 
+const content = {
+    footerText1: 'test',
+    footerText2: 'test',
+};
+
 it('Footer renders correctly', () => {
-    const tree = renderer.create(<Footer />).toJSON();
+    const tree = renderer.create(<Footer content={content} />).toJSON();
     expect(tree).toMatchSnapshot();
 });
