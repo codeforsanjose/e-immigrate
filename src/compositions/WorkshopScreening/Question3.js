29d49c95ce18437a589e90f144d55532bf56307d
Joseph Richardson <jrichardson@jmstudios.net>
2021-05-01 20:57:20 -0700

    bulma css calendar datepicker

--- src/compositions/WorkshopScreening/Question3.js
+++ src/compositions/WorkshopScreening/Question3.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../../components/Button/Button';
 import { format } from 'date-fns';
 import { enUS } from 'date-fns/locale';
-import { DatePicker } from 'react-nice-dates';
+
+import bulmaCalendar from 'bulma-calendar/dist/js/bulma-calendar.min';
+
+import 'bulma-calendar/dist/css/bulma-calendar.min.css';
 
 import 'react-nice-dates/build/style.css';
 
@@ -15,6 +18,36 @@ const Question3 = ({
     content,
     collectAnswer,
 }) => {
+    const showCalendar = useState(false);
+    useEffect(() => {
+        // Initialize all input of date type.
+        const calendars = bulmaCalendar.attach('[type="date"]', {
+            maxDate: new Date(),
+            isRange: false,
+            type: 'date',
+            enableYearSwitch: true,
+            closeOnSelect: true,
+            closeOnOverlayClick: true,
+        });
+
+        // Loop on each calendar initialized
+        calendars.forEach((calendar) => {
+            // Add listener to date:selected event
+            calendar.on('date:selected', (date) => {
+                console.log(date);
+            });
+        });
+
+        // To access to bulmaCalendar instance of an element
+        // eslint-disable-next-line no-undef
+        const element = document.querySelector('#dob');
+        if (element) {
+            // bulmaCalendar instance is available as element.bulmaCalendar
+            element.bulmaCalendar.on('select', (datepicker) => {
+                collectAnswer(datepicker.data.value());
+            });
+        }
+    }, []);
     if (q) {
         const onClick = (e) => {
             e.preventDefault();
@@ -24,36 +57,25 @@ const Question3 = ({
             );
             setShowModal(true);
         };
+        const calendarMarkup = showCalendar ? (
+            <input id="dob" type="date" />
+        ) : null;
+        const bulmaCssCalendar = (
+            <div className="bulma-calendar-container is-mobile">
+                {calendarMarkup}
+                <div className="RequiredError">*{content.errorMessage}</div>
+                <Button
+                    type="submit"
+                    label={content.screeningProceedButton}
+                    onClick={onClick}
+                />
+            </div>
+        );
+
         return (
             <div className="DatePicker">
                 <div className="QuestionText">{q.text}</div>
-                <div>
-                    <DatePicker
-                        date={date}
-                        onDateChange={setDate}
-                        locale={enUS}
-                    >
-                        {({ inputProps, focused }) => (
-                            <input
-                                type="date"
-                                name={q.slug}
-                                required={q.required}
-                                {...bindField(q.slug)}
-                                className={
-                                    ('input' + (focused ? ' -focused' : ''),
-                                    'TextInput')
-                                }
-                                {...inputProps}
-                            />
-                        )}
-                    </DatePicker>
-                    <div className="RequiredError">*{content.errorMessage}</div>
-                    <Button
-                        type="submit"
-                        label={content.screeningProceedButton}
-                        onClick={onClick}
-                    />
-                </div>
+                {bulmaCssCalendar}
             </div>
         );
     } else {
