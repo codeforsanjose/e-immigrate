b1e3a1ecba0a764b89cf048bf09b8b905365a82a
Logan Dang <logandang100@gmail.com>
2023-03-20 19:22:49 -0700

    npm update then prettier

--- backend/routes/questionnaireResponses.js
+++ backend/routes/questionnaireResponses.js
@@ -62,7 +62,8 @@ router.route('/').get((req, res) => {
 
 // source https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
--- src/containers/AdminDashboard/AdminDashboard.js
+++ src/containers/AdminDashboard/AdminDashboard.js
@@ -432,15 +432,17 @@ const AdminDashboard = (props) => {
             ];
             const allAnswers = Object.keys(questionnaireResponse).reduce(
                 (accumulator, questionKey, index) => {
-                    const flagIt = !questionKeysThatAreNotRedFlagsButInARedFlagQuestionnaire.includes(
-                        questionKey
-                    )
-                        ? questionnaireResponse[questionKey] &&
-                          questionnaireResponse[questionKey].toUpperCase() ===
-                              'YES'
-                            ? 'red-outline'
-                            : 'green-outline'
-                        : 'green-outline';
+                    const flagIt =
+                        !questionKeysThatAreNotRedFlagsButInARedFlagQuestionnaire.includes(
+                            questionKey
+                        )
+                            ? questionnaireResponse[questionKey] &&
+                              questionnaireResponse[
+                                  questionKey
+                              ].toUpperCase() === 'YES'
+                                ? 'red-outline'
+                                : 'green-outline'
+                            : 'green-outline';
                     const answerMarkup = !alreadyQuestionKeyMarkupedUp.includes(
                         questionKey
                     ) ? (
