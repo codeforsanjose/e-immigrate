2e8c639683559516ac1eefd9a7efed03b3ddc6e4
nick <nick.kats@gmail.com>
2024-03-21 09:41:16 -0700

    switch the phone number over to being a string

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
     {
         name: { type: String, required: false, unique: false },
-        phoneNumber: { type: Number, required: true, unique: true, trim: true },
+        phoneNumber: { type: String, required: true, unique: true, trim: true },
         document: { type: String, required: false, unique: false },
     },
     {
