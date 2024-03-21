0f3f177c28d4cbb1a0bc06b5fb0e06cc1b040d43
nick <nick.kats@gmail.com>
2024-03-21 09:36:52 -0700

    Added a table to store reports

---
+++ backend/src/models/excelReports.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { User } from './user.js';
+
+const Schema = mongoose.Schema;
+
+const excelReportSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    data: { type: Buffer, required: true, },
+    filename: { type: String, required: true, },
+    dateGenerated: Date,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: User }
+});
+
+export const ExcelReports = mongoose.model('ExcelReports', excelReportSchema);
+
+
