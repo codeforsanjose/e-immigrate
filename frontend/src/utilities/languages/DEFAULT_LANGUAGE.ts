5ba66875e9f5c9cb1c1a660b1322e36357b9eb74
nick <nick.kats@gmail.com>
2024-03-25 16:24:14 -0700

    Cleanup frontend rom merge

--- frontend/src/utilities/languages/DEFAULT_LANGUAGE.ts
+++
@@ -1,5 +0,0 @@
-export const DEFAULT_LANGUAGE = 'en' as const;
-export function getLanguageOrDefault<T extends string = string>(language: T) {
-    if (language == null || language === '') return DEFAULT_LANGUAGE;
-    return language;
-}
