6c8ee6ab450c8f12c2cd6e2f9a63f149ac934bc2
Joseph Richardson <richardson.joseph1990@gmail.com>
2024-05-23 10:28:52 -0700

    start of data and of routes for mers reporting

---
+++ backend/src/models/mersReportingQuestionnaireResponses.ts
@@ -0,0 +1,149 @@
+import mongoose from 'mongoose';
+import { WithDefaultMongooseId, WithMongooseTimestamps } from './core/types.js';
+
+/**
+ *   The fields listed here will be included in the
+ * generated excel files.
+ */
+export const fieldsExportableToExcel = [
+    'how_did_you_hear_about_event',
+    'age',
+    'birth_country',
+    'how_many_in_household',
+    'employment_status',
+    'household_income_monthly',
+    'highest_level_education',
+    'why_apply_for_citizenship',
+    'other_reason_for_citizenship',
+    'number_of_children_under_18_not_us_citizens',
+    'county_live_in_now',
+    'other_county_live_in',
+
+    'number_of_years_as_legal_permanant_resident',
+    'parents_us_citizen_before_your_18th_birthday',
+    'follow_up_parents_rule_out_acuisition',
+    'deemed_to_acquire_or_derived_citizenship',
+    'qualify_english_exemption',
+    'english_ready',
+    'last_5_years_single_trip_outside_us_longer_than_6months',
+    'qualify_fee_waiver',
+    'qualify_for_file_N400',
+    'follow_up_NO_qualify_for_N400',
+
+    'full_name',
+    'fee_waiver_status_application_complete',
+    'follow_up_if_not_eligible_fee_waiver',
+    'n400_status_report',
+    'follow_up_n400_status_report',
+    'understand_next_step',
+    'better_understanding_citizenship_process',
+    'participant_id_document',
+] satisfies Array<keyof MersReportingResponseEntity>;
+
+/**
+ *   NOTE: if you add a field here, and want it
+ * included in the generated excel files,
+ * add it to {@link fieldsExportableToExcel}
+ */
+export type MersReportingResponseEntity = {
+    how_did_you_hear_about_event: string;
+    age: string;
+    birth_country: string;
+    how_many_in_household: string;
+    employment_status: string;
+    household_income_monthly: string;
+    highest_level_education: string;
+    why_apply_for_citizenship: string;
+    other_reason_for_citizenship: string;
+    number_of_children_under_18_not_us_citizens: string;
+    county_live_in_now: string;
+    other_county_live_in: string;
+
+    number_of_years_as_legal_permanant_resident: string;
+    parents_us_citizen_before_your_18th_birthday: string;
+    follow_up_parents_rule_out_acuisition: string;
+    deemed_to_acquire_or_derived_citizenship: string;
+    qualify_english_exemption: string;
+    english_ready: string;
+    last_5_years_single_trip_outside_us_longer_than_6months: string;
+    qualify_fee_waiver: string;
+    qualify_for_file_N400: string;
+    follow_up_NO_qualify_for_N400: string;
+
+    full_name: string;
+    fee_waiver_status_application_complete: string;
+    follow_up_if_not_eligible_fee_waiver: string;
+    n400_status_report: string;
+    follow_up_n400_status_report: string;
+    understand_next_step: string;
+    better_understanding_citizenship_process: string;
+    participant_id_document: string;
+};
+
+
+export type MersReportingQuestionnaireResponseEntity = WithMongooseTimestamps<{
+    agency?: string;
+    mersReportingQuestionnaireResponse: MersReportingResponseEntity;
+    deleted?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
+}>;
+export type MersReportingQuestionnaireResponseEntityWithId = WithDefaultMongooseId<MersReportingQuestionnaireResponseEntity>;
+
+const responseSchema = new mongoose.Schema<MersReportingResponseEntity>({
+    how_did_you_hear_about_event: { type: String, required: false },
+    age: { type: String, required: false },
+    birth_country: { type: String, required: false },
+    how_many_in_household: { type: String, required: false },
+    employment_status: { type: String, required: false },
+    household_income_monthly: { type: String, required: false },
+    highest_level_education: { type: String, required: false },
+    why_apply_for_citizenship: { type: String, required: false },
+    other_reason_for_citizenship: { type: String, required: false },
+    number_of_children_under_18_not_us_citizens: { type: String, required: false },
+    county_live_in_now: { type: String, required: false },
+    other_county_live_in: { type: String, required: false },
+
+    number_of_years_as_legal_permanant_resident: { type: String, required: false },
+    parents_us_citizen_before_your_18th_birthday: { type: String, required: false },
+    follow_up_parents_rule_out_acuisition: { type: String, required: false },
+    deemed_to_acquire_or_derived_citizenship: { type: String, required: false },
+    qualify_english_exemption: { type: String, required: false },
+    english_ready: { type: String, required: false },
+    last_5_years_single_trip_outside_us_longer_than_6months: { type: String, required: false },
+    qualify_fee_waiver: { type: String, required: false },
+    qualify_for_file_N400: { type: String, required: false },
+    follow_up_NO_qualify_for_N400: { type: String, required: false },
+
+    full_name: { type: String, required: false },
+    fee_waiver_status_application_complete: { type: String, required: false },
+    follow_up_if_not_eligible_fee_waiver: { type: String, required: false },
+    n400_status_report: { type: String, required: false },
+    follow_up_n400_status_report: { type: String, required: false },
+    understand_next_step: { type: String, required: false },
+    better_understanding_citizenship_process: { type: String, required: false },
+    participant_id_document: { type: String, required: false },
+});
+
+
+
+const questionnaireSchema = new mongoose.Schema<MersReportingQuestionnaireResponseEntity>({
+    createdAt: { type: String, required: false },
+    updatedAt: { type: String, required: false },
+    agency: { type: String, required: false, unique: false },
+    mersReportingQuestionnaireResponse: { type: responseSchema, required: true },
+    deleted: {
+        type: Boolean,
+        required: false,
+        unique: false,
+        default: false,
+    },
+},
+{
+    timestamps: true,
+});
+
+export const MersReportingQuestionnaireResponse = mongoose.model<MersReportingQuestionnaireResponseEntity>(
+    'MersReportingQuestionnaireResponse',
+    questionnaireSchema
+);
---
+++ backend/src/routes/mersResportingResponseRouter.ts
@@ -0,0 +1,317 @@
+import express from 'express';
+import { z } from 'zod';
+
+import { MersReportingQuestionnaireResponse } from '../models/mersReportingQuestionnaireResponses.js';
+// import { QuestionnaireResponse } from '../models/questionnaireResponse.js';
+// import { sendMassEmails } from '../features/emails/sendEmail.js';
+// import { isEmailContentLanguage } from '../features/emails/emailContentLanguages.js';
+// import { emailContents } from "../features/emails/emailContents.js";
+import { authMiddleware } from '../middleware/authMiddleware.js';
+// import { emailSender } from '../features/emails/emailSender.js';
+// import { ArrayElementOf } from '../types/ArrayElementOf.js';
+// import { isSendGridResponseError } from '../types/SendGridResponseError.js';
+import { routeLogger } from '../features/logging/logger.js';
+// import { getUpdatedFlag } from './flagDeterminerHelpers.js';
+const router = express.Router();
+export { router as questionnaireResponsesRouter };
+const AddSchema = z.object({
+    title: z.string(),
+    language: z.string(),
+    questionnaireResponse: z.record(z.string(), z.string().nullable()),
+});
+
+// TODO: revisit access control
+router.route('/add').post(async (req, res) => {
+    const logger = routeLogger('addQuestionnaireResponse');
+
+    const reqBody = AddSchema.parse(req.body);
+    const {
+        language,
+        questionnaireResponse,
+        title,
+    } = reqBody;
+    logger.info({
+        origBody: req.body,
+        questionnaireResponse,
+    }, 'body');
+    const newQuestionnaireResponse = new MersReportingQuestionnaireResponse({
+        title,
+        language,
+        questionnaireResponse,
+    });
+
+    await newQuestionnaireResponse.save();
+    res.json('MersReportingQuestionnaireResponse response added');
+});
+
+router.use(authMiddleware); // all apis AFTER this line will require authentication as implemented in auth.js
+
+function getAllResponses() {
+    return MersReportingQuestionnaireResponse.find({
+        $or: [{ deleted: { $exists: false } }, { deleted: false }],
+    });
+}
+
+function getAllResponsesForIds(ids: Array<string>) {
+    return MersReportingQuestionnaireResponse.find({
+        $and: [
+            {
+                $or: [{ deleted: { $exists: false } }, { deleted: false }],
+            },
+            {
+                _id: { $in: ids },
+            },
+        ],
+    });
+}
+
+router.route('/').get(async (req, res) => {
+    const logger = routeLogger('getAllQuestionnaireResponse');
+    logger.trace('CALLED');
+    // const getResponses = req.user.issuper
+    //     ? getAllResponses()
+    //     : getResponsesForAdmin(req.user);
+
+    const qResponses = await getAllResponses();
+    // const updatedResponses = qResponses.filter((item) => {
+    //     return !(item.title?.toLowerCase().includes('spring_2021') ?? false);
+    // });
+    const responsesInfo = { responses: qResponses };
+    res.json(responsesInfo);
+});
+
+// source https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+// function validateEmail(email: string) {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
+
+// type QuestionnaireResponseElement = ArrayElementOf<Awaited<ReturnType<typeof getAllResponses>>>;
+
+// router.route('/email').post(async (req, res) => {
+//     const logger = routeLogger('emailQuestionnaireResponse');
+//     // const getResponses = req.user.issuper
+//     //     ? getAllResponses()
+//     //     : getResponsesForAdmin(req.user);
+//     const qResponses = await getAllResponses();
+//     const responsesToEmail = qResponses.filter((item) => item.emailSent !== true);
+//     const totalEmailsToSend = responsesToEmail.length;
+//     const messsagesToSend = responsesToEmail
+//         .filter((response) => {
+//             const {
+//                 questionnaireResponse,
+//                 language: responseLanguage = 'en',
+//             } = response;
+//             const { email = '' } = questionnaireResponse;
+//             if (!isEmailContentLanguage(responseLanguage)) return false;
+//             return email !== '' && validateEmail(email.toLowerCase());
+//         })
+//         .map((response) => {
+//             const {
+//                 questionnaireResponse,
+//                 flag,
+//                 language = 'en',
+//             } = response;
+//             if (!isEmailContentLanguage(language)) throw new Error('should not happen');
+
+//             const { email } = questionnaireResponse;
+//             const colorFlag = flag === true ? 'red' : 'green';
+//             const emailContentForResponse = emailContents[language] != null
+//                 ? emailContents[language][colorFlag]
+//                 : emailContents.en[colorFlag];
+//             const translatedContents =
+//                 (emailContentForResponse == null || emailContentForResponse.length === 0)
+//                     ? emailContents.en[colorFlag]
+//                     : emailContentForResponse;
+//             const msg = {
+//                 to: email.toLowerCase(),
+//                 from: emailSender,
+//                 subject: 'Your Response has been received',
+//                 html: translatedContents,
+//             };
+//             return msg;
+//         });
+
+//     try {
+//         await sendMassEmails(messsagesToSend);
+//         await updateUserResponsesEmailFlag(responsesToEmail, res);
+//     }
+//     catch (emailErrors) {
+//         logger.error(emailErrors, 'emails ERRORED');
+//         if (!isSendGridResponseError(emailErrors)) {
+//             throw emailErrors;
+//         }
+//         logger.error(emailErrors.response.body, 'em');
+//         res.json({
+//             msg: `Emails errored attempted to send ${totalEmailsToSend}`,
+//             errors: emailErrors.response.body,
+//         });
+//     }
+// });
+
+// async function updateUserResponsesEmailFlag(responsesToEmail: Array<QuestionnaireResponseElement>, res: express.Response) {
+//     const logger = scopedLogger('updateUserResponsesEmailFlag');
+//     const totalEmailsToSend = responsesToEmail.length;
+//     let emailsSentCurrent = 0;
+//     for (const response of responsesToEmail) {
+//         // pull out only what we need since coming from backend not frontne response contains lots more than what we need
+//         const {
+//             _id,
+//             title,
+//             language,
+//             flag,
+//             flagOverride = false,
+//             createdAt,
+//             updatedAt,
+//             questionnaireResponse,
+//             agency = '',
+//             responseDownloadedToExcel,
+//         } = response;
+//         const updatedFlag = flag ?? getUpdatedFlag(questionnaireResponse);
+//         const tempUpdatedSuccessEmail = {
+//             _id,
+//             title,
+//             emailSent: true,
+//             language,
+//             flag: updatedFlag,
+//             flagOverride,
+//             agency,
+//             questionnaireResponse,
+//             responseDownloadedToExcel,
+//             createdAt,
+//             updatedAt,
+//         };
+//         try {
+//             await MersReportingQuestionnaireResponse.updateOne(
+//                 { _id: response._id },
+//                 tempUpdatedSuccessEmail);
+//             emailsSentCurrent += 1;
+//             if (emailsSentCurrent === totalEmailsToSend) {
+//                 res.json({
+//                     msg: `Success attempted to send ${emailsSentCurrent}`,
+//                 });
+//             }
+//         }
+//         catch (mongoError) {
+//             logger.error(mongoError, 'mongo error here');
+//             res.json({
+//                 msg: `MongoError attempted to send ${emailsSentCurrent}`,
+//                 errors: mongoError,
+//             });
+//         }
+//     }
+// }
+
+const AssignAgencySchema = z.array(z.object({
+    id: z.string(),
+    agency: z.string().nullish(),
+}));
+
+router.route('/assign-agency').post(async (req, res) => {
+    const logger = routeLogger('agencyAssignURL');
+    const requestBody = AssignAgencySchema.parse(req.body);
+    for (const change of requestBody) {
+        try {
+            await MersReportingQuestionnaireResponse.updateOne({ _id: change.id }, {
+                agency: change.agency ?? null,
+            });
+        }
+        catch (err) {
+            logger.error({
+                request: change,
+                error: err,
+            }, 'Failed to update an agency');
+        }
+    }
+    logger.info(requestBody, 'Successfully assigned agency');
+    res.json(await getAllResponsesForIds(requestBody.map(x => x.id)));
+});
+
+const AssignFlagSchema = z.array(z.object({
+    id: z.string(),
+    flag: z.boolean().nullish(),
+}));
+
+router.route('/assign-flag').post(async (req, res) => {
+    const logger = routeLogger('assignResponseFlag');
+    const requestBody = AssignFlagSchema.parse(req.body);
+    for (const change of requestBody) {
+        try {
+            await MersReportingQuestionnaireResponse.updateOne({ _id: change.id }, {
+                flag: change.flag ?? null,
+            });
+        }
+        catch (err) {
+            logger.error({
+                request: change,
+                error: err,
+            }, 'Failed to update a flag');
+        }
+    }
+    logger.info(requestBody, 'Successfully assigned flag');
+    res.json(await getAllResponsesForIds(requestBody.map(x => x.id)));
+});
+
+// const AssignEmailSchema = z.array(z.object({
+//     id: z.string(),
+//     emailSent: z.boolean().nullish(),
+// }));
+
+// router.route('/assign-email').post(async (req, res) => {
+//     const logger = routeLogger('assignEmail');
+//     const requestBody = AssignEmailSchema.parse(req.body);
+
+//     for (const change of requestBody) {
+//         try {
+//             await MersReportingQuestionnaireResponse.updateOne({ _id: change.id }, {
+//                 emailSent: change.emailSent ?? false,
+//             });
+//         }
+//         catch (err) {
+//             logger.error({
+//                 request: change,
+//                 error: err,
+//             }, `Failed to update the 'email sent' field`);
+//         }
+//     }
+//     logger.info(requestBody, 'Successfully assigned emailSent');
+//     res.json(await getAllResponsesForIds(requestBody.map(x => x.id)));
+// });
+
+router.route('/:id').get(async (req, res) => {
+    const questionnaireResponse = await MersReportingQuestionnaireResponse.findById(req.params.id);
+    res.json(questionnaireResponse);
+});
+
+router.route('/:id').delete(async (req, res) => {
+    const logger = routeLogger('hardDeleteQuestionnaireResponse');
+    await MersReportingQuestionnaireResponse.findByIdAndDelete(req.params.id);
+    logger.info({
+        id: req.params.id,
+    }, 'questionnaire response Deleted');
+    res.json('questionnaire response Deleted');
+});
+
+// SOFT DELETE
+router.route('/delete/:id').put(async (req, res) => {
+    const logger = routeLogger('softDeleteQuestionnaireResponse');
+    const questionnaireResponse = await MersReportingQuestionnaireResponse.findByIdAndUpdate(
+
+        req.params.id,
+        { deleted: true });
+    if (questionnaireResponse == null) {
+        logger.error({
+            id: req.params.id,
+        }, 'Failed to find');
+        return;
+    }
+    logger.info({
+        paramId: req.params.id,
+        dbId: questionnaireResponse._id.toString(),
+    }, `questionnaire response soft-deleted`);
+    res.status(202).json({
+        msg: 'questionnaire response deleted softly',
+    });
+});
+
+router.route('');
